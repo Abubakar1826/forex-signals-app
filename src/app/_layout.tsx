c455e10b636bc01e6f636cfb6a7db7c80fed7d43
Abubakar1826 <abubakarmughal1826@gmail.com>
2026-09-21 12:34:56 +0500

    Remove unused import of AnimatedSplashOverlay

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import { Slot } from 'expo-router';
-
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Slot />
